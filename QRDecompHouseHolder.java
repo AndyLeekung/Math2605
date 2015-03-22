d975e13cc47dc0e507efe315011d182fbccda163
Andy Leekung <chingyeuleekung@gmail.com>
2015-03-22 18:30:35 -0400

    Finished making the QR with householder reflections

--- QRDecompHouseHolder.java
+++ QRDecompHouseHolder.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 /**
  * QR Decomposition using Householder reflections
  * @author Chingyeu Andy Leekung, Joeseph Lesniak, Jacob Goodpasture
@@ -6,16 +7,24 @@
 public class QRDecompHouseHolder {
 	
 	//TODO EVERYTHING
+	/**
+	 * Matrix being passed in
+	 */
+	private Matrix A;
 	
+	private ArrayList<Matrix> hhMatrices;
+	private Matrix Q, R;
 	/**
 	 * Storage for the matrices of the QR decomp
 	 */
-	private double[][] QR, Q, R;
+	//private double[][] QR, Q, R;
 	
 	/**
 	 * Row and column dimensions
 	 */
-	private int m, n;
+	private static int m;
+
+	private static int n;
 	
 	/* ------------------------
 	   Constructors
@@ -27,10 +36,35 @@ public class QRDecompHouseHolder {
 	 * @param A Matrix to decompose
 	 */
 	public QRDecompHouseHolder(Matrix A) {
-		QR = A.getArrayCopy();
+		this.A = A;
+		this.R = A;
+		//QR = A.getArrayCopy();
 		m = A.getRowDimension();
-		n = A.getRowDimension();
+		n = A.getColumnDimension();
+		hhMatrices = new ArrayList<Matrix>();
 		//TODO Do the QR stuff here
+		for (int i = 0; i < n - 1; i++) {
+			//look at the column vectors along the diagonal
+			double[] colVec = R.getColumnVector(i, i);
+			//check if the vector needs a reflection applied to it
+			if (!checkZeroes(colVec)) {
+				double[] hhVec = houseHolderVector(colVec);
+				Matrix hhMat = houseHolderMatrix(hhVec);
+				//hhMat.print(5, 5);
+				hhMatrices.add(hhMat);
+				R = hhMat.multiply(R);
+			}
+			//R = R.uminus();
+		}
+		this.Q = hhMatrices.get(hhMatrices.size() - 1);
+		//Q.print(5, 6);
+		for (int i = hhMatrices.size() - 2; i >= 0; i--) {
+			Matrix h = hhMatrices.get(i);
+			//h.print(5, 6);
+			Q =  h.multiply(Q);
+		}
+		Q = Q.uminus();
+		R = R.uminus();
 	}
 	
 	/* ------------------------
@@ -42,7 +76,7 @@ public class QRDecompHouseHolder {
 	 * @return Matrix Q
 	 */
 	public Matrix getQ() {
-		return new Matrix(Q);
+		return this.Q;
 	}
 	
 	/**
@@ -50,15 +84,19 @@ public class QRDecompHouseHolder {
 	 * @return Matrix R
 	 */
 	public Matrix getR() {
-		return new Matrix(R);
+		return this.R;
 	}
+
+	/* ------------------------
+	   Private Methods
+	 * ------------------------ */
 	
 	/**
 	 * Private method to get the householder vector
 	 * @param vector Vector to reflect
 	 * @return the householder reflection vector
 	 */
-	public static double[] houseHolderVector(double[] x) {
+	private static double[] houseHolderVector(double[] x) {
 		double[] houseHolder = new double[x.length];
 		double[] e = new double[x.length];
 		double normX = Matrix.norm(x);
@@ -78,17 +116,41 @@ public class QRDecompHouseHolder {
 	 * @param hhVec The householder vector 
 	 * @return The householder matrix
 	 */
-	public static Matrix houseHolderMatrix(double[] hhVec) {
+	private static Matrix houseHolderMatrix(double[] hhVec) {
 		//u * u^t
 		Matrix X = Matrix.multiplyVectors(hhVec, hhVec);
 		//2 * u * u^t
 		X.timesEquals(2);
 		//I
 		Matrix houseHolder = Matrix.identity(X.getRowDimension(), X.getColumnDimension());
-		//houseHolder.print(5, 2);
 		// I - 2uu^t
 		houseHolder.minusEquals(X);
+		//houseHolder = houseHolder.uminus();
+		//put matrix into the same sized matrix as original if it is smaller than original
+		int hhM = houseHolder.getRowDimension();
+		int hhN = houseHolder.getColumnDimension();
+		if (m != hhM || n != hhN) {
+			Matrix I = Matrix.identity(m, n);
+			I.setMatrix(m - hhM, m - 1, n - hhN, n - 1, houseHolder);
+			houseHolder = I;
+		}
+		
 		return houseHolder;
+	}
+	
+	/**
+	 * Checks if vector has zeroes below first term
+	 * @param vector Vector to check
+	 * @return True if vector has all zeroes below first term, else false
+	 */
+	private boolean checkZeroes(double[] vector) {
+		boolean hasZeroes = true;
+		for (int i = 1; i < vector.length && hasZeroes == true; i++) {
+			if (vector[i] != 0) {
+				hasZeroes = false;
+			}
+		}
+		return hasZeroes;
 	}
 
 }
