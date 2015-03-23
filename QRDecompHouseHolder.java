27d857939dd59b1928af30c97bf0aef059882e2e
Andy Leekung <chingyeuleekung@gmail.com>
2015-03-23 18:38:07 -0400

    Changed to reflect new abstract class

--- QRDecompHouseHolder.java
+++ QRDecompHouseHolder.java
@@ -4,28 +4,9 @@ import java.util.ArrayList;
  * @author Chingyeu Andy Leekung, Joeseph Lesniak, Jacob Goodpasture
  * @version 1.0
  */
-public class QRDecompHouseHolder {
+public class QRDecompHouseHolder extends QRDecomp{
 	
-	//TODO EVERYTHING
-	/**
-	 * Matrix being passed in
-	 */
-	private Matrix A;
-	
-	private ArrayList<Matrix> hhMatrices;
-	private Matrix Q, R;
-	/**
-	 * Storage for the matrices of the QR decomp
-	 */
-	//private double[][] QR, Q, R;
-	
-	/**
-	 * Row and column dimensions
-	 */
-	private static int m;
-
-	private static int n;
-	
+		
 	/* ------------------------
 	   Constructors
 	 * ------------------------ */
@@ -36,12 +17,12 @@ public class QRDecompHouseHolder {
 	 * @param A Matrix to decompose
 	 */
 	public QRDecompHouseHolder(Matrix A) {
-		this.A = A;
+		//this.A = A;
 		this.R = A;
 		//QR = A.getArrayCopy();
-		m = A.getRowDimension();
-		n = A.getColumnDimension();
-		hhMatrices = new ArrayList<Matrix>();
+		this.m = A.getRowDimension();
+		this.n = A.getColumnDimension();
+		qMatrices = new ArrayList<Matrix>();
 		//TODO Do the QR stuff here
 		for (int i = 0; i < n - 1; i++) {
 			//look at the column vectors along the diagonal
@@ -51,16 +32,13 @@ public class QRDecompHouseHolder {
 				double[] hhVec = houseHolderVector(colVec);
 				Matrix hhMat = houseHolderMatrix(hhVec);
 				//hhMat.print(5, 5);
-				hhMatrices.add(hhMat);
+				qMatrices.add(hhMat);
 				R = hhMat.multiply(R);
 			}
-			//R = R.uminus();
 		}
-		this.Q = hhMatrices.get(hhMatrices.size() - 1);
-		//Q.print(5, 6);
-		for (int i = hhMatrices.size() - 2; i >= 0; i--) {
-			Matrix h = hhMatrices.get(i);
-			//h.print(5, 6);
+		this.Q = qMatrices.get(qMatrices.size() - 1);
+		for (int i = qMatrices.size() - 2; i >= 0; i--) {
+			Matrix h = qMatrices.get(i);
 			Q =  h.multiply(Q);
 		}
 		Q = Q.uminus();
@@ -71,21 +49,12 @@ public class QRDecompHouseHolder {
 	   Public Methods
 	 * ------------------------ */
 	
-	/**
-	 * Return Q of the QR decomposition
-	 * @return Matrix Q
-	 */
-	public Matrix getQ() {
-		return this.Q;
-	}
 	
-	/**
-	 * Return R of the QR decomposition 
-	 * @return Matrix R
-	 */
-	public Matrix getR() {
-		return this.R;
-	}
+	//Solve Ax = b
+	//QRx = b
+	//Qy = b
+	//Rx = y
+	//Q^tb = y
 
 	/* ------------------------
 	   Private Methods
@@ -125,7 +94,6 @@ public class QRDecompHouseHolder {
 		Matrix houseHolder = Matrix.identity(X.getRowDimension(), X.getColumnDimension());
 		// I - 2uu^t
 		houseHolder.minusEquals(X);
-		//houseHolder = houseHolder.uminus();
 		//put matrix into the same sized matrix as original if it is smaller than original
 		int hhM = houseHolder.getRowDimension();
 		int hhN = houseHolder.getColumnDimension();
@@ -137,20 +105,6 @@ public class QRDecompHouseHolder {
 		
 		return houseHolder;
 	}
-	
-	/**
-	 * Checks if vector has zeroes below first term
-	 * @param vector Vector to check
-	 * @return True if vector has all zeroes below first term, else false
-	 */
-	private boolean checkZeroes(double[] vector) {
-		boolean hasZeroes = true;
-		for (int i = 1; i < vector.length && hasZeroes == true; i++) {
-			if (vector[i] != 0) {
-				hasZeroes = false;
-			}
-		}
-		return hasZeroes;
-	}
+
 
 }
